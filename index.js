b022cbecc87c6bcaa7294af8559e93cc81159079
Alaif <mohammed.alaif@skill-mine.com>
2024-02-21 17:50:22 +0530

    changed port value for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Define a route for GET requests to /ping
 app.get('/ping', (req, res) => {
